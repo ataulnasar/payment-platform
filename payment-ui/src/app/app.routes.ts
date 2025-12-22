b1cb0e8227c35c212b6d894f55877161d1e2cf30
kareerodev <ataulnasar@gmail.com>
2025-12-22 18:51:45 +0100

    Wire routing

--- payment-ui/src/app/app.routes.ts
+++ payment-ui/src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { PaymentComponent } from './pages/payment/payment.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: PaymentComponent },
+];
